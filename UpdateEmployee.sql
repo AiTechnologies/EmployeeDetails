ad297c28164460fb9fb9d6372bb116c7ce9b150c
AiTechnologies <aitechnologies.tn@gmail.com>
2017-07-18 17:27:42 +0530

    Delete UpdateEmployee.sql

--- UpdateEmployee.sql
+++
@@ -1,9 +0,0 @@
-CREATE PROCEDURE EpdateEmployee
-(
-EmployeeNo  int(11), 
-EmployeeName varchar(255),
-
-Sex varchar(10)
-)
-
-Update Employee.EmployeeDetails set Sex="Male" where EmployeeNo=EmployeeNo
